cae37207cb67035a3c0d75b16093eee5697f544a
Jahfar Muhammed <jahfarbinmuhammed117@gmail.com>
2026-08-13 17:27:16 +0530

    Owl filter list view completed

--- custom_addons/filter_tree_view/static/src/views/list_view.js
+++ custom_addons/filter_tree_view/static/src/views/list_view.js
@@ -1,36 +1,64 @@
 /** @odoo-module **/
-import { registry } from '@web/core/registry';
-import { listView } from '@web/views/list/list_view';
-import { ListController } from '@web/views/list/list_controller';
-import { useState } from "@odoo/owl";
-import { SearchBar } from "@web/search/search_bar/search_bar";
-import { useService } from "@web/core/utils/hooks";
-export class SalesPersonFilter extends ListController {
-    setup(){
+// import { ListController } from '@web/views/list/list_controller';
+import {onWillStart} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
+import {patch} from "@web/core/utils/patch";
+import {ListRenderer} from "@web/views/list/list_renderer";
+// export class SalesPersonFilter extends ListController {
+//     setup(){
+//         super.setup();
+//         this.state = useState({salesPerson : null});
+//         this.orm = useService("orm");
+//         this.action = useService("action");
+//         this.searchBar = SearchBar;
+//         this.salesPersons =[];
+//         onWillStart(async()=>{
+//             this.salesPersons = await this.orm.searchRead("res.users",[],['id','name']);
+//         });
+//     }
+//     async onChange(event){
+//         this.state.salesPerson = parseInt(event.currentTarget.value);
+//         await this.env.searchModel.clearQuery();
+//         if(this.state.salesPerson) {
+//             await this.env.searchModel.splitAndAddDomain(`[("user_id", "in", [${this.state.salesPerson}])]`);
+//         }
+//     }
+// }
+// registry.category('views').add('sale_person_filter', {
+//     ...listView,
+//      Controller:SalesPersonFilter
+// });
+// patch(ListController.prototype, {
+//     setup(){
+//         super.setup()
+//         this.salesPersons = [];
+//         this.orm = useService("orm");
+//         onWillStart(async () => {
+//             this.salesPersons = await this.orm.searchRead("res.users", [], ['id', 'name']);
+//         });
+//     },
+//     async onChange(event) {
+//         this.salesPerson = parseInt(event.currentTarget.value);
+//         await this.env.searchModel.clearQuery();
+//         if (this.salesPerson) {
+//             await this.env.searchModel.splitAndAddDomain(`[("user_id", "in", [${this.salesPerson}])]`);
+//         }
+//     }
+// });
+patch(ListRenderer.prototype, {
+    setup() {
         super.setup();
-        this.state = useState({salesPerson : null});
-        this.orm = useService("orm");
-        this.action = useService("action");
-        this.searchBar = SearchBar
-        this.salesPersons =[]
-        this.fetchSalesPersons();
+        onWillStart(async () => {
+            if(this.env.searchModel.resModel==='crm.lead') {
+                this.salesPersons = await this.orm.searchRead("res.users", [], ['id', 'name']);
+            }
+        });
+    },
+    async onChange(event) {
+        this.salesPerson = parseInt(event.currentTarget.value);
+        await this.env.searchModel.clearQuery();
+        if (this.salesPerson) {
+            await this.env.searchModel.splitAndAddDomain(`[("user_id", "in", [${this.salesPerson}])]`);
+        }
     }
-
-    async fetchSalesPersons(){
-       this.salesPersons = await this.orm.searchRead("res.users",[],['id','name']);
-    }
-    async onChange(event){
-        this.state.salesPerson = event.currentTarget.value;
-        // this.model.env.searchModel.domain =[
-        //     // ...this.model.env.searchModel.domain,
-        //     // ["user_id",'=',this.state.salesPerson]
-        // ]
-        console.log(this.searchBar.set)
-        console.log(ListController.components.SearchBar)
-        console.log(this.model.env.searchModel.domain);
-    }
-}
-registry.category('views').add('sale_person_filter', {
-    ...listView,
-     Controller:SalesPersonFilter
 });
